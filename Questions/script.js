d0e385df27c57a6f5e018e94baf9c3f8e7fae30f
M3353 <55003556+M3353@users.noreply.github.com>
2021-02-24 01:14:22 -0500

    Added questions project

---
+++ Questions/script.js
@@ -0,0 +1,29 @@
+// // TRAVERSING THE DOM
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//     // e is event object
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//     });
+// });
+
+
+// USING SELECTORS INSIDE OF THE ELEMENT
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+
+        questions.forEach(function(item){
+            if (item != question)
+            {
+                item.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
